25b568eacca43ff3df55cb11b41017f661cbb810
TulioMoura <90994955+TulioMoura@users.noreply.github.com>
2022-01-26 05:37:16 -0300

    Alterações dia 2 e desafios

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 //componente react
 import appConfig from '../config.json'
 import {Box, Button, Text, TextField,Image} from '@skynexui/components';
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+import React from 'react';
+import {useRouter} from 'next/router'
+
 function Title(props){
     const Tag = props.tag || 'h1';
   return (
@@ -65,17 +40,22 @@ function Title(props){
 
  export default HomePage*/
 
- export default function PaginaInicial() {
-    const username = 'TulioMoura';
-  
+ export default  function  PaginaInicial() {
+    //const username = 'TulioMoura';
+    const [username, setUserName] = React.useState('');
+   const [githubInfo, setGithubInfo] = React.useState('');
+    const roteamento = useRouter()
+     
+    
+                   
+   
     return (
       <>
-        <GlobalStyle />
+        
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
-            backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            backgroundImage: 'url(https://cdn.pixabay.com/photo/2017/12/16/11/47/waters-3022515_960_720.jpg)',
             backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           }}
         >
@@ -92,23 +72,55 @@ function Title(props){
               borderRadius: '10px', padding: '32px', margin: '16px',
               boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
               backgroundColor: appConfig.theme.colors.neutrals[700],
+              opacity: 0.9,
             }}
           >
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={ function(e){
+                e.preventDefault();
+                {roteamento.push('/chat')}
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
               }}
             >
               <Title tag="h2">Boas vindas de volta!</Title>
-              <Text variant="body4" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+              <Text variant="body4" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300],}}>
                 {appConfig.name}
               </Text>
   
-              <TextField
-                fullWidth
+             
+               
+              
+              
+           
+              <TextField value={username}
+              fullWidth
+              onChange={async function(e){
+                let user= e.target.value
+                let userlenght= user.length;
+                setUserName(user);
+                    console.log(`https://api.github.com/users/${user}`)
+                    await fetch(`https://api.github.com/users/${user}`)
+                  .then(function (response){
+                   
+                    if(!response.ok){
+                      return console.log("Erro! ", response.status)
+                    }
+                    setGithubInfo(JSON.stringify(response)) 
+                  console.log(githubInfo)
+
+                  })
+                  
+                      
+                
+                  
+                           
+              }
+            }
                 textFieldColors={{
                   neutral: {
                     textColor: appConfig.theme.colors.neutrals[200],
@@ -117,15 +129,17 @@ function Title(props){
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
                 }}
+                
               />
               <Button
                 type='submit'
                 label='Entrar'
+                
                 fullWidth
                 
                 buttonColors={{
                   contrastColor: appConfig.theme.colors.neutrals["000"],
-                  mainColor: appConfig.theme.colors.primary[500],
+                  mainColor: appConfig.theme.colors.primary[400],
                   mainColorLight: appConfig.theme.colors.primary[400],
                   mainColorStrong: appConfig.theme.colors.neutrals[800],
                 }}
@@ -151,7 +165,9 @@ function Title(props){
                 minHeight: '240px',
               }}
             >
-              <Image
+             {username.length>2?(
+             <>
+             <Image
                 styleSheet={{
                   borderRadius: '50%',
                   marginBottom: '16px',
@@ -168,8 +184,11 @@ function Title(props){
                 }}
               >
                 {username}
-              </Text>
+                
+              </Text></>
+            ):(<></>)}
             </Box>
+            
             {/* Photo Area */}
           </Box>
         </Box>
